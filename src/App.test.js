b8f0b4b06dcba193eb54ee7c8d97bfe0a370bd41
ramesh inumula <inumularamesh@gmail.com>
2025-03-05 20:40:37 +0530

    Form with basic fields

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,18 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  // RENDER THE PAGE
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText("Form in React");
+  const nameElement = screen.getByPlaceholderText("Enter First Name");
+  const lastNameElement = screen.getByPlaceholderText("Enter Last Name");
+  const emailElement = screen.getByPlaceholderText("Enter email");
+  const mblElement = screen.getByPlaceholderText("Enter Mobile number");
+  const genElement = screen.getByText("Gender*");
+  expect(nameElement).toBeInTheDocument();
+  expect(emailElement).toBeInTheDocument();
+  expect(mblElement).toBeInTheDocument();
+  expect(lastNameElement).toBeInTheDocument();
+  expect(titleElement).toBeInTheDocument();
+  expect(genElement).toBeInTheDocument();
 });
